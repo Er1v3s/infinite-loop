7eabb0cc5fb6d86dd9fd35590dc8ba82dc174414
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2022-11-02 21:39:18 +0100

    Stylize header, create menu, mixins and animations
    CSS:
    - Create mixins and animations, use it in header
    - Improvement RWD work
    - Create wide screens linear menu in nav
    - Create and stylize drop-down mobile menu
    JS:
    - Add event listener on burger-icon click to drop-down menu
    - Resolve problem running scripts before DOM Loaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./scss/style.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const burgerMenu = document.querySelector(".nav__options");
+  const burgerBtn = document.querySelector(".nav__burger--mobile");
+
+  burgerBtn.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+  });
+});
