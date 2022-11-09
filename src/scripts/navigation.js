1b2fa4f9ac09963ed6bbc8a9b29b0b0a165d5559
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2022-11-09 03:56:58 +0100

    Fix scroll navbar on widther devices
    - Change nav__options-option font color on scroll from white
      to basic blue
    - Add subtelHov transition

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,6 +1,7 @@
 const navHandle = document.querySelector(".header__nav");
 const burgerBtnHandle = document.querySelector(".nav__burger-mobile");
 const burgerMenu = document.querySelector(".nav__options");
+const navOptionHandle = document.querySelectorAll(".nav__options-option");
 
 window.addEventListener("DOMContentLoaded", () => {
   burgerBtnHandle.addEventListener("click", () => {
@@ -12,8 +13,14 @@ window.addEventListener("scroll", () => {
   if (scrollY > 166) {
     navHandle.classList.add("header__nav--active");
     burgerBtnHandle.classList.add("nav__burger-mobile--active");
+    navOptionHandle.forEach((item) => {
+      item.classList.add("nav__options-option--active");
+    });
   } else {
     navHandle.classList.remove("header__nav--active");
     burgerBtnHandle.classList.remove("nav__burger-mobile--active");
+    navOptionHandle.forEach((item) => {
+      item.classList.remove("nav__options-option--active");
+    });
   }
 });
