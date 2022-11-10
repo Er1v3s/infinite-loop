bb31e90d71d6e4e7ab61a020873af3bfa917c0f0
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2022-11-10 23:38:05 +0100

    Start creating slider for reference-section
    - Create separate script file for new script
    - Stylize and fix issue with slider reference-section (overflow)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./scss/style.scss";
 import "./scripts/navigation.js";
+import "./scripts/reference-section.js";
--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,9 +2,6 @@ const navHandle = document.querySelector(".header__nav");
 const burgerBtnHandle = document.querySelector(".nav__burger-mobile");
 const burgerMenu = document.querySelector(".nav__options");
 const navOptionHandle = document.querySelectorAll(".nav__options-option");
-const aboutSectionHandle = document.querySelector(".about-section");
-const referenceSectionHandle = document.querySelector(".reference-section");
-const style = document.head.appendChild(document.createElement("style"));
 
 window.addEventListener("DOMContentLoaded", () => {
   burgerBtnHandle.addEventListener("click", () => {
@@ -25,33 +22,5 @@ window.addEventListener("scroll", () => {
     navOptionHandle.forEach((item) => {
       item.classList.remove("nav__options-option--active");
     });
-  }
-});
-
-window.addEventListener("scroll", () => {
-  if (scrollY + window.innerHeight >= referenceSectionHandle.offsetTop) {
-    referenceSectionHandle.classList.add("reference-section--active");
-  } else if (scrollY + window.innerHeight < referenceSectionHandle.offsetTop) {
-    referenceSectionHandle.classList.remove("reference-section--active");
-  }
-
-  if (scrollY >= referenceSectionHandle.offsetTop - 165) {
-    style.innerHTML = `.reference-section--active::before {height: 100px !important;}`;
-  } else if (scrollY < referenceSectionHandle.offsetTop - 165) {
-    style.innerHTML = `.reference-section--active::before {height: 0px !important;}`;
-  }
-
-  if (
-    scrollY + window.innerHeight >
-    referenceSectionHandle.offsetTop + referenceSectionHandle.offsetHeight + 100
-  ) {
-    style.innerHTML = `.reference-section--active::after {height: 0 !important;} .reference-section--active::before {height: 100px !important;}`;
-  }
-
-  if (
-    scrollY >
-    referenceSectionHandle.offsetTop + referenceSectionHandle.offsetHeight + 100
-  ) {
-    referenceSectionHandle.classList.remove("reference-section--active");
   }
 });
