ea72317e32502d356c2e505784216fe8288f7604
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2022-11-18 17:46:22 +0100

    Create navbar links to page elements
    - create links in navigation, unset them default style properties
    - add style changer in js
    - set scroll-behaviour for html selector

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -8,6 +8,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const navHandle = document.querySelector(".header__nav");
   const burgerBtnHandle = document.querySelector(".nav__burger-mobile");
   const burgerMenu = document.querySelector(".nav__options");
+  const linkMenu = this.document.querySelectorAll(".nav__options-link");
 
   burgerBtnHandle.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
@@ -16,9 +17,15 @@ window.addEventListener("DOMContentLoaded", function () {
     if (scrollY >= 1) {
       navHandle.classList.add("header__nav--active");
       burgerBtnHandle.classList.add("nav__burger-mobile--active");
+      linkMenu.forEach((item) => {
+        item.classList.add("nav__options-link--active");
+      });
     } else {
       navHandle.classList.remove("header__nav--active");
       burgerBtnHandle.classList.remove("nav__burger-mobile--active");
+      linkMenu.forEach((item) => {
+        item.classList.remove("nav__options-link--active");
+      });
     }
   });
 });
